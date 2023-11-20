89264cd2488eae38d3bdb4fce5bfc91e708c1f84
Jan Szkaradek <js7656@gmail.com>
2023-11-20 13:35:57 +0000

    added query parser

---
+++ src/main/java/QueryParser.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class QueryParser {
+    public static String[][] loadQueries(String absQueryPath) throws IOException{
+        List<String[]> queryDocList = new ArrayList<>();
+
+        File topicFile = new File(absQueryPath);
+
+        if(topicFile == null){
+            throw new IOException("Query Parser Error: File not found");
+        }
+
+        Document queryContent = Jsoup.parse(topicFile, null, "");
+
+        Elements docs = queryContent.select("top");
+
+        for(Element doc : docs){
+            String title, desc, narr;
+            title = (doc.select("title").text());
+            desc = (doc.select("desc").text());
+            narr = (doc.select("narr").text());
+            queryDocList.add(new String[]{title, desc, narr});
+        }
+
+        String[][] queryTopicArray = new String[queryDocList.size()][];
+        queryTopicArray = queryDocList.toArray(queryTopicArray);
+
+        return queryTopicArray;
+    }
+}
